330d7a15a34971a4661b48e6489804e9cd8ff2fa
Priyanshu Agarwal <priyanshu.agarwal88@gmail.com>
2020-06-21 01:45:58 +0530

    animated choose word dialog

--- lib/pages/chooseWord.dart
+++ lib/pages/chooseWord.dart
@@ -5,8 +5,7 @@ import 'room/room.dart';
 import 'dart:math';
 import 'painterScreen.dart';
 import 'selectRoom.dart';
-
-//bool wc = false;
+import 'dart:async';
 
 class ChooseWordDialog extends StatefulWidget {
   @override
@@ -17,11 +16,21 @@ class _ChooseWordDialogState extends State<ChooseWordDialog> {
 List displayWords = [' ', ' ', ' '];
 
 Color wordBack;
-
 Color wordText;
+
+bool switcher=false;
+
+double topPadding= totalLength*0.8;
+Curve curve= Curves.elasticOut; 
+Duration duration = Duration(milliseconds: 1000);
 @override
   void initState() {
         getWords();
+         WidgetsBinding.instance.addPostFrameCallback((_) {
+           setState(() {
+             topPadding=totalLength*0.1;
+           });
+                });
     super.initState();
   }
   @override
@@ -30,76 +39,104 @@ Color wordText;
     wordBack = null;
     // wordText=Color(0xFF1A2F77);
     wordText = Colors.black;
-    return Container(
-      // decoration: BoxDecoration(
-      //     image: DecorationImage(
-      //         fit: BoxFit.cover,
-      //         colorFilter: new ColorFilter.mode(
-      //             Colors.black.withOpacity(0.4), BlendMode.dstATop),
-      //         image: AssetImage('assets/images/pencil.jpg'))),
-      // color: Colors.white,
-      color: Colors.white,
-      constraints: BoxConstraints.expand(),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          Text(
-            'Pick a word',
-            style: GoogleFonts.notoSans(color: Colors.black, fontSize: 25.0),
+    return Stack(
+      children: [
+        Container(
+          color: Colors.white,
+          constraints: BoxConstraints.expand(),
+        ),
+        AnimatedPositioned(
+          duration: duration,
+          top:topPadding,
+          left: totalWidth*0.15,
+          curve: curve,
+          onEnd: (){
+            if(topPadding!=totalLength*0.1)
+            updateWord();
+            },
+                  child: Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Text(
+                'Pick a word',
+                style: GoogleFonts.notoSans(color: Colors.black, fontSize: 25.0),
+              ),
+              SizedBox(height: totalLength*0.05,),
+              Container(
+                  height: 200.0,
+                  //color: Colors.red,
+                  child: Column(
+                    children: <Widget>[
+                      FlatButton(
+                        color: wordBack,
+                        shape: new RoundedRectangleBorder(
+                            borderRadius: new BorderRadius.circular(10.0)),
+                        child: Text(
+                          displayWords[0].toUpperCase(),
+                          style:
+                              GoogleFonts.poppins(color: wordText, fontSize: 18.0),
+                        ),
+                        onPressed: () {
+                          choosenWord = displayWords[0];
+                          //updateWord();
+                          setState(() {
+                            topPadding=totalLength*0.8;
+                            curve = Curves.easeInBack;
+                            duration= Duration(milliseconds: 500);
+                          });
+                        },
+                      ),
+                      FlatButton(
+                        color: wordBack,
+                        shape: new RoundedRectangleBorder(
+                            borderRadius: new BorderRadius.circular(10.0)),
+                        child: Text(
+                          displayWords[1].toUpperCase(),
+                          style:
+                              GoogleFonts.poppins(color: wordText, fontSize: 18.0),
+                        ),
+                        onPressed: () {
+                          choosenWord = displayWords[1];
+                          //updateWord();
+                           setState(() {
+                            topPadding=totalLength*0.8;
+                            curve = Curves.easeInBack;
+                            duration= Duration(milliseconds: 500);
+
+                          });
+                        },
+                      ),
+                      FlatButton(
+                        color: wordBack,
+                        shape: new RoundedRectangleBorder(
+                            borderRadius: new BorderRadius.circular(10.0)),
+                        child: Text(
+                          displayWords[2].toUpperCase(),
+                          style:
+                              GoogleFonts.poppins(color: wordText, fontSize: 18.0),
+                        ),
+                        onPressed: () {
+                          choosenWord = displayWords[2];
+                          //updateWord();
+                           setState(() {
+                            topPadding=totalLength*0.8;
+                            curve = Curves.easeInBack;
+                            duration= Duration(milliseconds: 500);
+
+                          });
+                        },
+                      ),
+                      SizedBox(height: totalLength*0.03,),
+                     TimeIndicator()
+                    ],
+                  ))
+            ],
           ),
-          Container(
-              height: 200.0,
-              //color: Colors.red,
-              child: Column(
-                children: <Widget>[
-                  FlatButton(
-                    color: wordBack,
-                    shape: new RoundedRectangleBorder(
-                        borderRadius: new BorderRadius.circular(10.0)),
-                    child: Text(
-                      displayWords[0].toUpperCase(),
-                      style:
-                          GoogleFonts.poppins(color: wordText, fontSize: 18.0),
-                    ),
-                    onPressed: () {
-                      choosenWord = displayWords[0];
-                      updateWord();
-                    },
-                  ),
-                  FlatButton(
-                    color: wordBack,
-                    shape: new RoundedRectangleBorder(
-                        borderRadius: new BorderRadius.circular(10.0)),
-                    child: Text(
-                      displayWords[1].toUpperCase(),
-                      style:
-                          GoogleFonts.poppins(color: wordText, fontSize: 18.0),
-                    ),
-                    onPressed: () {
-                      choosenWord = displayWords[1];
-                      updateWord();
-                    },
-                  ),
-                  FlatButton(
-                    color: wordBack,
-                    shape: new RoundedRectangleBorder(
-                        borderRadius: new BorderRadius.circular(10.0)),
-                    child: Text(
-                      displayWords[2].toUpperCase(),
-                      style:
-                          GoogleFonts.poppins(color: wordText, fontSize: 18.0),
-                    ),
-                    onPressed: () {
-                      choosenWord = displayWords[2];
-                      updateWord();
-                    },
-                  ),
-                 TimeIndicator()
-                ],
-              ))
-        ],
       ),
+        ),
+      ],
     );
 
     //
@@ -150,7 +187,12 @@ class TimeIndicatorState extends State<TimeIndicator> with TickerProviderStateMi
     super.initState();
     _colorTween = ColorTween(begin: green, end: red)
         .animate(timeIndicator);
+        Timer(
+        Duration(
+          milliseconds: 800,
+        ), () {
         timeIndicator.forward(from:0.0);
+        });
   }
   @override
   void dispose() {
@@ -159,22 +201,24 @@ class TimeIndicatorState extends State<TimeIndicator> with TickerProviderStateMi
   }
   @override
   Widget build(BuildContext context) {
-    return   Container(
-                    width:width,
-                    alignment: Alignment.centerLeft,
-                    child: AnimatedBuilder(
-                      animation: timeIndicator,
-                       builder:(context,child){
-                        return Container(                    
-                        height: 12.0,
-                       // width: animatedWidth,
-                       width: (1-timeIndicator.value) *width,
-                        decoration: BoxDecoration(
-                            color: _colorTween.value,
-                            border: Border.all(color: _colorTween.value),
-                            borderRadius: BorderRadius.circular(15.0)),
-                      );}
+    return   RepaintBoundary(
+          child: Container(
+                      width:width,
+                      alignment: Alignment.centerLeft,
+                      child: AnimatedBuilder(
+                        animation: timeIndicator,
+                         builder:(context,child){
+                          return Container(                    
+                          height: 12.0,
+                         // width: animatedWidth,
+                         width: (1-timeIndicator.value) *width,
+                          decoration: BoxDecoration(
+                              color: _colorTween.value,
+                              border: Border.all(color: _colorTween.value),
+                              borderRadius: BorderRadius.circular(15.0)),
+                        );}
+                      ),
                     ),
-                  );
+    );
   }
 }
